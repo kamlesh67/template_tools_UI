9de4b24ac5fe05c1a73a733a38774c24c77dbd10
sagar-kumar-mz <98985336+sagar-kumar-mz@users.noreply.github.com>
2022-05-03 10:22:51 +0530

    ids added to replace from api side

--- sessionTool.js
+++ sessionTool.js
@@ -28,7 +28,9 @@ const speakerList = function (speakers) {
     }
   });
   if (speakers.length > 4) {
-    sps += `<div style="background-color:${theme.accentColor}; color:${theme.secondaryFontColor};height: 30px;width: 30px;overflow: hidden;border-radius: 30px; display: flex; justify-content: center;align-items: center;font-size: 13px;">
+    sps += `<div style="background-color:${theme.accentColor}; color:${
+      theme.secondaryFontColor
+    };height: 30px;width: 30px;overflow: hidden;border-radius: 30px; display: flex; justify-content: center;align-items: center;font-size: 13px;">
               +${speakers.length - 4}
             </div>`;
   }
@@ -45,7 +47,9 @@ const boothList = function (booths) {
     }
   });
   if (booths.length > 2) {
-    bts += `<div style="background-color:${theme.accentColor};color:${theme.secondaryFontColor};height: 30px;width: 30px;overflow: hidden;border-radius: 4px; display: flex; justify-content: center;align-items: center;font-size: 13px;"> 
+    bts += `<div style="background-color:${theme.accentColor};color:${
+      theme.secondaryFontColor
+    };height: 30px;width: 30px;overflow: hidden;border-radius: 4px; display: flex; justify-content: center;align-items: center;font-size: 13px;"> 
                +${booths.length - 2}
             </div>`;
   }
@@ -67,18 +71,26 @@ const speakerAndBoothList = function (values, isPreview) {
   if (values?.speakers?.length || values?.booths?.length || !values?.sessionLibrary?.selected?.id) {
     if (isPreview) {
       return ` 
-      <div style="display: flex;justify-content: start;align-items: center;" id="${values?.sessionLibrary?.selected?.id}-sessionSpeaker">
+      <div style="display: flex;justify-content: start;align-items: center;" id="${
+        values?.sessionLibrary?.selected?.id
+      }-sessionSpeakers">
       ${!values?.sessionLibrary?.selected?.id ? defaultSpeaker : speakerList(values?.speakers)} 
       </div>
-      <div style="display: flex;justify-content: end;align-items: center;"  id="${values?.sessionLibrary?.selected?.id}-sessionBooth">
+      <div style="display: flex;justify-content: end;align-items: center;"  id="${
+        values?.sessionLibrary?.selected?.id
+      }-sessionBooths">
       ${!values?.sessionLibrary?.selected?.id ? defaultBooth : boothList(values?.booths)}
       </div>`;
     } else {
       return ` 
-      <div style="display: flex;justify-content: end;align-items: center;" id="${values?.sessionLibrary?.selected?.id}-sessionSpeaker">
+      <div style="display: flex;justify-content: end;align-items: center;" id="${
+        values?.sessionLibrary?.selected?.id
+      }-sessionSpeakers">
       ${!values?.sessionLibrary?.selected?.id ? defaultSpeaker : speakerList(values?.speakers)} 
       </div>
-      <div style="display: flex;justify-content: end;align-items: center;"  id="${values?.sessionLibrary?.selected?.id}-sessionBooth">
+      <div style="display: flex;justify-content: end;align-items: center;"  id="${
+        values?.sessionLibrary?.selected?.id
+      }-sessionBooths">
       ${!values?.sessionLibrary?.selected?.id ? defaultBooth : boothList(values?.booths)}
       </div>`;
     }
@@ -123,16 +135,21 @@ const modalTemplate = function (data) {
 };
 
 const toolTemplate = function (values, isViewer = false) {
-  if(values.sessionLibrary)
-  {
-  return `<div class="session-card" style="position:relative;background-color:${values.sessionCardBGColor}" >
+  if (values.sessionLibrary) {
+    return `<div class="session-card" style="position:relative;background-color:${
+      values.sessionCardBGColor
+    }" >
     <div class="session-card-body">
       <p class="session-date" style="color:${values.sessionDateAndTimeColor};">
       <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none">
       <path d="M8.25 0.5C6.76664 0.5 5.3166 0.939867 4.08323 1.76398C2.84986 2.58809 1.88856 3.75943 1.32091 5.12987C0.75325 6.50032 0.604725 8.00832 0.894114 9.46318C1.1835 10.918 1.89781 12.2544 2.9467 13.3033C3.9956 14.3522 5.33197 15.0665 6.78683 15.3559C8.24168 15.6453 9.74968 15.4968 11.1201 14.9291C12.4906 14.3614 13.6619 13.4001 14.486 12.1668C15.3101 10.9334 15.75 9.48336 15.75 8C15.75 7.01509 15.556 6.03982 15.1791 5.12987C14.8022 4.21993 14.2497 3.39314 13.5533 2.6967C12.8569 2.00026 12.0301 1.44781 11.1201 1.0709C10.2102 0.693993 9.23492 0.5 8.25 0.5V0.5ZM8.25 14C7.06332 14 5.90328 13.6481 4.91658 12.9888C3.92989 12.3295 3.16085 11.3925 2.70673 10.2961C2.2526 9.19974 2.13378 7.99334 2.36529 6.82946C2.5968 5.66557 3.16825 4.59647 4.00736 3.75736C4.84648 2.91824 5.91558 2.3468 7.07946 2.11529C8.24335 1.88378 9.44975 2.0026 10.5461 2.45672C11.6425 2.91085 12.5795 3.67988 13.2388 4.66658C13.8981 5.65327 14.25 6.81331 14.25 8C14.25 9.5913 13.6179 11.1174 12.4926 12.2426C11.3674 13.3679 9.8413 14 8.25 14V14ZM8.25 3.5C8.05109 3.5 7.86033 3.57902 7.71967 3.71967C7.57902 3.86032 7.5 4.05109 7.5 4.25V7.9475L6.4725 9.725C6.37305 9.89805 6.34641 10.1035 6.39845 10.2962C6.45049 10.4889 6.57695 10.653 6.75 10.7525C6.92306 10.852 7.12853 10.8786 7.32123 10.8266C7.51392 10.7745 7.67805 10.6481 7.7775 10.475L8.9025 8.525C8.97466 8.38943 9.00346 8.23497 8.985 8.0825L9 8V4.25C9 4.05109 8.92099 3.86032 8.78033 3.71967C8.63968 3.57902 8.44892 3.5 8.25 3.5Z" fill="currentcolor"/>
-      </svg> <span style="margin-left:10px"> ${values.dateAndTime ? values.dateAndTime : 'session date and time | session timezone'} </span>
+      </svg> <span style="margin-left:10px"> ${
+        values.dateAndTime ? values.dateAndTime : 'session date and time | session timezone'
+      } </span>
       </p>
-      <h3 class="session-title" style="margin: 10px 0 5px 0; color: ${values.sessionNameColor};">${values.sessionName ? values.sessionName : 'Session Name'}</h3>
+      <h3 class="session-title" style="margin: 10px 0 5px 0; color: ${values.sessionNameColor};">${
+      values.sessionName ? values.sessionName : 'Session Name'
+    }</h3>
       <p class="session-description" style="color:${values.sessionDescriptionColor};">
         ${values.description ? values.description : 'session description'}
       </p>
@@ -142,9 +159,8 @@ const toolTemplate = function (values, isViewer = false) {
     </div>
   </div>
   ${isViewer ? modalTemplate({ sessions: values.data.sessions }) : ''}`;
-    }
-    else{
-      return `
+  } else {
+    return `
       <div style="position:relative;background-color:#F6F8F8;border:1px solid rgba(0,0,0,.125);border-radius:4px;margin:auto;text-align:center; padding:20px 10px;">
       <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 48 48" fill="none">
         <path d="M38 8H10C7.79086 8 6 9.79086 6 12V40C6 42.2091 7.79086 44 10 44H38C40.2091 44 42 42.2091 42 40V12C42 9.79086 40.2091 8 38 8Z" stroke="#C0C0C0" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
@@ -155,25 +171,38 @@ const toolTemplate = function (values, isViewer = false) {
       <p style="font-size:13px;color:#808080;">Click here to select a community banner from the list.</p>
       </div>
       ${isViewer ? modalTemplate({ sessions: values.data.sessions }) : ''}`;
-    }
+  }
 };
 
 const toolEmailTemplate = function (values, isViewer = false) {
-  if(values.sessionLibrary)
-  {
-  return `
-  <div style="position:relative;background-color:${values.sessionCardBGColor};margin-bottom: 15px;height: auto;padding: 14px;border-radius: 8px; max-width:540px;" >
+  if (values.sessionLibrary) {
+    return `
+  <div sessionId="${
+    values?.sessionLibrary?.selected?.id
+  }" style="position:relative;background-color:${
+      values.sessionCardBGColor
+    };margin-bottom: 15px;height: auto;padding: 14px;border-radius: 8px; max-width:540px;" >
   <div class="session-card-body">
-    <p style="color:${values.sessionDateAndTimeColor};display: flex;align-items: center;font-size: 12px;font-weight: 500;">
+    <p style="color:${
+      values.sessionDateAndTimeColor
+    };display: flex;align-items: center;font-size: 12px;font-weight: 500;">
     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none">
     <path d="M8.25 0.5C6.76664 0.5 5.3166 0.939867 4.08323 1.76398C2.84986 2.58809 1.88856 3.75943 1.32091 5.12987C0.75325 6.50032 0.604725 8.00832 0.894114 9.46318C1.1835 10.918 1.89781 12.2544 2.9467 13.3033C3.9956 14.3522 5.33197 15.0665 6.78683 15.3559C8.24168 15.6453 9.74968 15.4968 11.1201 14.9291C12.4906 14.3614 13.6619 13.4001 14.486 12.1668C15.3101 10.9334 15.75 9.48336 15.75 8C15.75 7.01509 15.556 6.03982 15.1791 5.12987C14.8022 4.21993 14.2497 3.39314 13.5533 2.6967C12.8569 2.00026 12.0301 1.44781 11.1201 1.0709C10.2102 0.693993 9.23492 0.5 8.25 0.5V0.5ZM8.25 14C7.06332 14 5.90328 13.6481 4.91658 12.9888C3.92989 12.3295 3.16085 11.3925 2.70673 10.2961C2.2526 9.19974 2.13378 7.99334 2.36529 6.82946C2.5968 5.66557 3.16825 4.59647 4.00736 3.75736C4.84648 2.91824 5.91558 2.3468 7.07946 2.11529C8.24335 1.88378 9.44975 2.0026 10.5461 2.45672C11.6425 2.91085 12.5795 3.67988 13.2388 4.66658C13.8981 5.65327 14.25 6.81331 14.25 8C14.25 9.5913 13.6179 11.1174 12.4926 12.2426C11.3674 13.3679 9.8413 14 8.25 14V14ZM8.25 3.5C8.05109 3.5 7.86033 3.57902 7.71967 3.71967C7.57902 3.86032 7.5 4.05109 7.5 4.25V7.9475L6.4725 9.725C6.37305 9.89805 6.34641 10.1035 6.39845 10.2962C6.45049 10.4889 6.57695 10.653 6.75 10.7525C6.92306 10.852 7.12853 10.8786 7.32123 10.8266C7.51392 10.7745 7.67805 10.6481 7.7775 10.475L8.9025 8.525C8.97466 8.38943 9.00346 8.23497 8.985 8.0825L9 8V4.25C9 4.05109 8.92099 3.86032 8.78033 3.71967C8.63968 3.57902 8.44892 3.5 8.25 3.5Z" fill="currentcolor"/>
-    </svg> <span style="margin-left:10px"> ${
-      values.dateAndTime ? values.dateAndTime : 'session date and time | session timezone'
-    } </span>
+    </svg> <span id="${
+      values?.sessionLibrary?.selected?.id
+    }-sessionDateAndTime" style="margin-left:10px"> ${values?.dateAndTime} </span>
     </p>
-    <h3 style="margin: 10px 0 5px 0; color: ${values.sessionNameColor};font-size: 14px;font-weight: bold;white-space: nowrap;overflow: hidden;text-overflow: ellipsis;display: block;">${values.sessionName ? values.sessionName : 'Session Name'}</h3>
-    <p style="color:${values.sessionDescriptionColor};font-size: 12px;font-weight: 400;white-space: nowrap;overflow: hidden;text-overflow: ellipsis;display: block;">
-      ${values.description ? values.description : 'session description'}
+    <h3 id="${
+      values?.sessionLibrary?.selected?.id
+    }-sessionName" style="margin: 10px 0 5px 0; color: ${
+      values.sessionNameColor
+    };font-size: 14px;font-weight: bold;white-space: nowrap;overflow: hidden;text-overflow: ellipsis;display: block;">${
+      values?.sessionName
+    }</h3>
+    <p id="${values?.sessionLibrary?.selected?.id}-sessionDescription" style="color:${
+      values.sessionDescriptionColor
+    };font-size: 12px;font-weight: 400;white-space: nowrap;overflow: hidden;text-overflow: ellipsis;display: block;">
+      ${values?.description}
     </p>
     <div style="display:flex;justify-content: space-between;margin-top:10px;">
       ${values.isShowSpeakerAndBooth ? speakerAndBoothList(values) : ''}  
@@ -181,9 +210,7 @@ const toolEmailTemplate = function (values, isViewer = false) {
   </div>
 </div>
   `;
-  }
-  else
-  {
+  } else {
     return ``;
   }
 };
@@ -265,7 +292,7 @@ unlayer.registerPropertyEditor({
 unlayer.registerTool({
   name: 'session_tool',
   label: 'Session',
-  icon: 'fa-calendar', 
+  icon: 'fa-calendar',
   supportedDisplayModes: ['web', 'email'],
   options: {
     boothContent: {
