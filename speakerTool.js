134509c82a30e4d2ed31783adcec6a868d4df26b
sagar-kumar-mz <98985336+sagar-kumar-mz@users.noreply.github.com>
2022-06-03 13:01:56 +0530

    Update speakerTool.js

--- speakerTool.js
+++ speakerTool.js
@@ -176,7 +176,6 @@
             /* Register event listeners for search */
             const searchBar = document.querySelector('#search-bar-speaker');
             searchBar.onkeydown = function (e) {
-console.log('e?.which', searchBar.value.toLowerCase(), e.target.value)
               if(e?.which === 13 || !e.target.value || (e.target.value.length === 1 && e?.which === 8)){ 
                 const list = document.querySelector('#speaker_library_modal .speakers-list');
                 let filteredItem;
