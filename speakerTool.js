c259cf2102d0230e93fb90a557528495e3e5a6d1
Gautam Makasana <gautam@HUBLAPIND-191.local>
2022-06-07 16:03:55 +0530

    Speaker UI fixes

--- speakerTool.js
+++ speakerTool.js
@@ -98,7 +98,7 @@
     </style>
     <div  speakerId="${
       values?.speakerLibrary?.selected?.id
-    }" style="max-width: 115px; position:relative; margin-bottom: 15px;height: 210px;padding-top:0px;overflow: hidden;border-radius: 8px;background-color:"${values?.speakerBGColor}";box-sizing: border-box;"> 
+    }" style="max-width: 115px; position:relative; margin-bottom: 15px;height: 210px;padding-top:0px;overflow: hidden;border-radius: 8px;background-color:${values?.speakerBGColor}; box-sizing: border-box;"> 
     <div class="speaker-img" style="position: relative; z-index: 1; overflow: hidden; border-radius: 20rem 0 20rem 20rem; border: 1rem solid #E67E23; top: -21px; right: -16px; line-height:0;">
     <img id="${values?.speakerLibrary?.selected?.id}-speakerImg" src="${values?.speakerImage?.url}" alt="image" style="height:85px; width: 85px; object-fit:cover; position: relative; top: 5px;" />
     </div>
