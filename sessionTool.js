eaec942cf055c165dc32b666a8d1d5ceca872830
sagar-kumar-mz <98985336+sagar-kumar-mz@users.noreply.github.com>
2022-06-07 18:41:24 +0530

    Update sessionTool.js

--- sessionTool.js
+++ sessionTool.js
@@ -246,6 +246,41 @@
   };
 
   unlayer.registerPropertyEditor({
+  name: 'my_color_picker',
+  Widget: unlayer.createWidget({
+    render(value, updateValue, data) {
+      return `
+        <input class="color-value" value=${value} />
+        <button class="red">Red</button>
+        <button class="green">Green</button>
+        <button class="blue">Blue</button>
+      `
+    },
+    mount(node, value, updateValue, data) {
+      var input = node.getElementsByClassName('color-value')[0]
+      input.onchange = function(event) {
+        updateValue(event.target.value)
+      }
+
+      var redButton = node.getElementsByClassName('red')[0]
+      redButton.onclick = function() {
+        updateValue('#f00')
+      }
+
+      var greenButton = node.getElementsByClassName('green')[0]
+      greenButton.onclick = function() {
+        updateValue('#0f0')
+      }
+
+      var blueButton = node.getElementsByClassName('blue')[0]
+      blueButton.onclick = function() {
+        updateValue('#00f')
+      }
+    }
+  })
+});
+
+  unlayer.registerPropertyEditor({
     name: 'session_library',
     layout: 'bottom',
     Widget: unlayer.createWidget({
@@ -355,7 +390,7 @@
  position: 0,
             label: 'Session Background Color',
             defaultValue: theme?.primaryColor,
-            widget: 'color_picker',
+            widget: 'my_color_picker',
           },
           sessionNameColor: {
             enabled: false,
