539f514a95dff600dc25f33ac786cc0b0a7a6866
Gautam Makasana <gautam@HUBLAPIND-191.local>
2022-06-09 11:45:45 +0530

    Speaker re-design

--- speakerTool.js
+++ speakerTool.js
@@ -5,12 +5,8 @@
   <% _.forEach(speakers, function(item) { %>
     <div class="speakers-item card" id="speakers-item" data-uuid='<%= item.id %>' data-title="<%= item.name %>" data-designation="<%= item.designation %>" data-image="<%= item.profile_img %>" data-company="<%= item.company %>" >
     <div class="speakers-media"> <img src="<%= item.profile_img %>" alt="image" style="height:auto; width: 100%; object-fit:cover; border-radius: 8px;" /> </div>
-      <h3 style="style="margin:5px 8px 0; font-size:13px; color: ${
-        values.speakerTitleColor
-      };overflow: hidden;  display: block;  text-overflow: ellipsis;  white-space: nowrap;"><%= item?.name %> </h4>
-      <h4 style="margin:5px 8px 0; font-size:12px; height: 35px; overflow:hidden; color: ${
-        values.speakerDesignationCompanyColor
-      };"><%= item?.designation %> <%= item?.designation && item?.company ? ',' : '' %> <%= item?.company %> </h5>
+      <h3 style="margin:5px 10px 0; text-align: left; color: ${theme.primary};overflow: hidden;  display: block;  text-overflow: ellipsis;  white-space: nowrap;"><%= item?.name %> </h4>
+      <h5 style="margin:5px 10px 0; text-align: left;color: ${theme.secondary};"><%= item?.designation %> <%= item?.designation && item?.company ? ',' : '' %> <%= item?.company %> </h5>
     </div>
   <% }); %>
 `);
